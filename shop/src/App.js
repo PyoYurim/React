96b11764b931fd2416a87cea8b2c82caef7e1723
PyoYurim <pyoqhfk1225@naver.com>
2023-05-18 15:31:58 +0900

    Dosc : 기능에 따른 주석 추가

--- shop/src/App.js
+++ shop/src/App.js
@@ -14,7 +14,7 @@ import axios from 'axios'
 export let Context1 = createContext()
 
 function App() {
-
+  //data.js에서 정보 가져오기
   let [shoes, setShoes] = useState(data)
   let [재고] = useState([10, 11, 12])
 
@@ -24,11 +24,14 @@ function App() {
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
+          {/* Shop클릭 시 home으로 */}
           <Navbar.Brand href="#home">Shop</Navbar.Brand>
           <Nav className="me-auto">
             {/* <Link to="/" style={{textDecoration:"none", marginLeft:20}}>홈</Link>
             <Link to="/detail" style={{textDecoration:"none", marginLeft:40}}>상세페이지</Link> */}
+            {/* Nav를 이용하여 스타일 지정 */}
             <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            {/* Detail클릭 시 /detail경로로 이동하게 해주는거 */}
             <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
             <Nav.Link onClick={()=>{navigate('/mypage')}}>My Page</Nav.Link>
           </Nav>
@@ -46,23 +49,27 @@ function App() {
                 </div>
               </div>
               <button onClick={()=>{
+                //axios.get을 이용해서 데이터를 가져온다
                 axios.get('https://codingapple1.github.io/shop/data2.json')
+                //setShoes 함수를 이용하여 shoes배열에 결과.data 배열을 합칩니다.
                 .then((결과)=>{ 
                   setShoes(shoes.concat(결과.data)) 
                 })
               }}>버튼</button>
             </>
           }/>
-          
+          {/* URL 경로 설정하는 속성, :id는 매개변수 역할을 수행하며 동적인 값을 가질 수 있는 경로 */}
           <Route path="/detail/:id" element={
             <Context1.Provider value={{ 재고, shoes }}>
+              {/* Detail 컴포넌트를 렌더링하는 부분으로 shoes라는 props를 전달하여 Detail 컴포넌트에서 해당 데이터를 사용할 수 있도록 하기 위함이다 */}
               <Detail shoes={shoes} />
             </Context1.Provider>
           } />
         {/* 404 페이지 */}
         <Route path="*" element={<div>없는 페이지요</div>} />
         <Route path="/mypage" element={<MyPage></MyPage>}/>
-
+        
+        {/* /about/member 이런식으로 검색하면 멤버임이 화면에 나온다 */}
         <Route path="/about" element={<About />}>
           <Route path="member" element={<div>멤버임</div>} /> {/* 어디에 보여줄지 정하기 위해 <Outlet></Outlet>사용 */}
           <Route path="location" element={<div>위치정보임</div>} />
@@ -81,6 +88,7 @@ function Event(){
   return(
     <div>
       <h4>오늘의 이벤트</h4>
+      {/* one, two를 경로표시할때 나타나게 하기 위해서 */}
       <Outlet></Outlet>
     </div>
   )
--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -23,16 +23,18 @@ function Detail(props) {
   const [alert1, setAlert1] = useState(true);
   let [탭, 탭변경] = useState(0);
   const [count, setCount] = useState(0);
+  // URL의 매개변수 값 추출 후, props.shoes 배열에서 해당 매개변수 값과 일치하는 첫번째 상품을 찾은 상품 변수에 할당한다
   const { id } = useParams();
   const 찾은상품 = props.shoes.find(function (x) {
     return x.id == id;
   });
-
+  //useEffect()훅을 사용하여 num변수의 변경 감지, 이에 따른 다양한 동작 수행
   useEffect(() => {
+  //2초 후에 실행되는 타임아웃 설정, 2초 후에 setAlter1(false)를 호출하여 alert1 상태 변경
     let timeout = setTimeout(() => {
       setAlert1(false);
     }, 2000);
-
+    //isNan()함수는 전달된 값이 숫자인지 확인하는 함수, num이 숫자가 아닌 경우 그러지마세요 경고 메세지를 띄우는 역할
     if (isNaN(num)) {
       alert('그러지마세요');
     }
@@ -42,6 +44,7 @@ function Detail(props) {
 
   return (
     <div className="container">
+      {/* alert1이 true인 경우 2초 이내 구매 시 할인이 뜨고 2초후 false로 바뀌므로 null로 바뀐다 */}
       {alert1 ? (
         <div className="alert alert-warning">2초 이내 구매 시 할인</div>
       ) : null}
@@ -52,6 +55,7 @@ function Detail(props) {
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" alt="shoes" />
         </div>
         <div className="col-md-6">
+          {/* 사용자의 입력을 실시간으로 감지하고 상태 업데이트 */}
           <input onChange={(e) => setNum(e.target.value)} />
           <h4 className="pt-5">{찾은상품.title}</h4>
           <p>{찾은상품.content}</p>
@@ -80,16 +84,20 @@ function TabContent({ 탭 }) {
   let [fade, setFade] = useState('');
 
   useEffect(() => {
+    //0.1초 후에 실행되는 타임아웃 설정
     setTimeout(() => {
       setFade('end');
     }, 100);
+    //useEffect()의 반환 함수를 사용하여 정리 함수 제공 마운트가 해제되거나 업데이트되기 전에 실행
     return () => {
       setFade('');
     };
   }, [탭]);
 
   return (
+    //start와 fade의 상태 값을 조합하여 클래스명 생성 fade값이 end일 경우 startend클래스 적용 그렇지 않을 경우 start클래스 적용
     <div className={'start' + fade}>
+      {/* 배열을 사용하여 탭 값에 따라 내용을 렌더링하는 부분 배열의 인데스에 탭 값을 넣어 해당하는 내용 선택, 에를 들어 탭 값이 0이면 첫번째 내용0이 렌더링된다 */}
       {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][탭]}
     </div>
   );
